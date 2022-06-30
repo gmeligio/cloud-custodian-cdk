2d771ab7bdd9cce9b293814d088c20607b6ddc70
Eligio Mariño <eligio.marino@cmrad.com>
2022-07-01 00:34:12 +0200

    build: remove ts-node

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
-import * as schema from './generated/schema'
+// import * as schema from './'
 
 class MyStack extends TerraformStack {
 	constructor(scope: Construct, name: string) {
 		super(scope, name);
 
 		// define resources here
-		schema.
+		// schema.
 	}
 }
 
